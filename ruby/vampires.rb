096247410ef6f7d9653d0a0655eda6fddfaa4c27
Kit Robinson <krobinson.dev@gmail.com>
2016-05-24 11:48:38 -0500

    added questions and inputs to vampires.rb

---
+++ ruby/vampires.rb
@@ -0,0 +1,29 @@
+#Variables
+name = nil
+age = nil
+birthYear = nil
+wantsGarlicBread = nil
+wantsHealthInsurance = nil
+
+#Start
+puts "Welcome to the automated Werewolves Inc. HR system!"
+puts "Please answer the following questions:"
+puts "What is your name?"
+name = gets.chomp
+puts "Thanks #{name}, now what is your age?"
+age = gets.chomp.to_i
+puts "Excellent... and what year were you born?"
+birthYear = gets.chomp.to_i
+puts "Great work.  Incidentally, our company cafeteria has garlic bread today, should we order some for you? (Y/N)"
+if gets.chomp == 'Y'
+	wantsGarlicBread = true
+else
+	wantsGarlicBread = false
+end
+puts "Ok.  And speaking of things you might want, would you like to enroll in the company's health insurance? (Y/N)"
+if gets.chomp == 'Y'
+	wantsHealthInsurance = true
+else
+	wantsHealthInsurance = false
+end
+puts "Thank you for filling out the employee data sheet!"
