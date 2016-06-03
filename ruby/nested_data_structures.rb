c13a8146fbca68c1d95b1f0c2fbec4fa8264b360
Kit Robinson <krobinson.dev@gmail.com>
2016-06-03 12:16:41 -0500

    add a few calls to show deeply nested data

--- ruby/nested_data_structures.rb
+++ ruby/nested_data_structures.rb
@@ -40,4 +40,33 @@ restaurant = {
 	}
 }
 puts "Restaurant Initialized!"
-
+puts "Example table 6:"
+puts "    Server:"
+puts restaurant[:nonsmoking_section][:table_6][:server]
+puts "    Guests:"
+puts restaurant[:nonsmoking_section][:table_6][:guests][0..-1]
+puts "    Seating used at table_6:"
+puts "#{restaurant[:nonsmoking_section][:table_6][:guests].length} out of #{restaurant[:nonsmoking_section][:table_6][:seats]}"
+puts ""
+puts "    Total count of guests at restaurant:"
+guestcount = 0
+restaurant.each do |key, value|
+	if value.is_a?(Array)
+		guestcount += value.length
+	end
+	if value.is_a?(Hash)
+		value.each do |key2, value2|
+			if value2.is_a?(Array)
+				guestcount += value2.length
+			end
+			if value2.is_a?(Hash)
+				value2.each do |key3, value3|
+					if value3.is_a?(Array)
+						guestcount += value3.length
+					end
+				end
+			end
+		end
+	end
+end
+puts guestcount
