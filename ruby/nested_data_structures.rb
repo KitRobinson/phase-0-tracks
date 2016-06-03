c41e71744f1e76b1da4968dc64e71b9711c987c9
Kit Robinson <krobinson.dev@gmail.com>
2016-06-03 11:46:09 -0500

    add nested data to nested-data_structures.rb... and also hamster.rb, which I can't seem to remove?

---
+++ ruby/nested_data_structures.rb
@@ -0,0 +1,43 @@
+restaurant = {
+	smoking_section: {
+		table_1: {
+			seats: 6,
+			server: "Adam",
+			guests: ["Gideon, Jace, Chandra"]
+		},
+		table_2: {
+			seats: 4,
+			server: "Adam",
+			guests: ["Paula", "Cathy", "Richard", "Lindsay"]
+		},
+		table_3: {
+			seats: 4,
+			server: "Bruce",
+			guests: []
+		}
+	},
+	nonsmoking_section: {
+		table_4: {
+			seats: 6,
+			server: "Bruce",
+			guests: ["James", "Charles", "Charlie", "Jim"]
+		},
+		table_5: {
+			seats: 4,
+			server: "Bruce",
+			guests: ["Julius", "Octavian", "Nero", "Augustus"]
+		},
+		table_6: {
+			seats: 8,
+			server: "Adam",
+			guests: ["James", "Leonard", "Hikaru", "Pavel", "Montomery", "Nyota", "Spock"]
+		}
+	},
+	bar_section: {
+		seats: 5,
+		server: "Charlene",
+		guests: ["Jack", "Jim", "James", "Johnny"]
+	}
+}
+puts "Restaurant Initialized!"
+
