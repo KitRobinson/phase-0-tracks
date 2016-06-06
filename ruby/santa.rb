725d464ea3423acc9f1ed6384643311f61a2a8b4
Kit Robinson <krobinson.dev@gmail.com>
2016-06-06 17:10:14 -0500

    add some getter and setter methods to santa class

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -16,6 +16,29 @@ class Santa
 		@reindeer_ranking = ["Rudolph", "Dasher", "Dancer", "Prancer", "Vixen", "Comet", "Cupid", "Donner", "Blitzen"]
 	end
 
+	def celebrate_birthday
+		@age += 1
+	end
+
+	def get_mad_at(reindeer)
+		if @reindeer_ranking.index(reindeer) != nil
+			@reindeer_ranking.delete_at(@reindeer_ranking.index(reindeer))
+			@reindeer_ranking << reindeer
+		end
+	end
+
+	def age
+		@age
+	end
+
+	def ethnicity
+		@ethnicity
+	end
+
+	def reindeer_ranking
+		@reindeer_ranking
+	end
+
 end
 
 primeSanta = Santa.new("Male", "Caucasian")
@@ -24,3 +47,10 @@ primeSanta.eat_milk_and_cookies("chocolate chip cookie")
 
 santa2 = Santa.new("Female", "Hispanic")
 santa3 = Santa.new("In Transition", "Klingon")
+
+puts primeSanta.age
+primeSanta.celebrate_birthday
+puts primeSanta.age
+puts primeSanta.reindeer_ranking
+primeSanta.get_mad_at("Rudolph")
+puts primeSanta.reindeer_ranking
