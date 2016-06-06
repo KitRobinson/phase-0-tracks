694bcc771d2071f51de3b6cbc09cdc64073f2eee
Kit Robinson <krobinson.dev@gmail.com>
2016-06-06 14:44:40 -0500

    add puppy class design to puppy.rb

---
+++ ruby/puppy.rb
@@ -0,0 +1,25 @@
+=begin
+	
+SPECIES---------------------
+Canis Canis (classname 'Puppy')
+
+CHARACTERISTICS-------------
+Breed: Varies
+Age: Varies
+Coat: Varies
+Markings: Varies
+Cuteness: true
+Sire: Varies
+Dam: Vaires
+
+BEHAVIORS-------------------
+Speak
+Play_Dead
+Roll_Over
+Shake
+Sit
+Stay
+Down
+Come
+
+end
