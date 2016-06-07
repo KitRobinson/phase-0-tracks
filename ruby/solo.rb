ea8483c443688daf50dd731c3fd5f7dc20b5b2de
Kit Robinson <krobinson.dev@gmail.com>
2016-06-07 15:10:36 -0500

    add and test work, pay, and assign supervisor methods

--- ruby/solo.rb
+++ ruby/solo.rb
@@ -15,13 +15,14 @@ employee ID
 wages owed
 
 methods of employees------------ 
-update info
+update info (covered in part by accessor methods)
 work
 collect pay
 assign to supervisor
 
 =end
 $nextID = 1
+$employeeDirectory = []
 
 class Employee
 
@@ -40,6 +41,7 @@ class Employee
 		@directReports = []
 		@pay = 10
 		@wages = 0
+		$employeeDirectory << self
 	end
 
 	def about
@@ -54,6 +56,29 @@ class Employee
 		puts "currently owed: $" + @wages.to_s + " in wages"  
 	end
 
+	def work(hours)
+		puts @first + " works for " + hours.to_s + " hours"
+		@wages = @wages + (hours * @pay)
+		puts "and is now owed $" + @wages.to_s
+	end
+
+	def collectPay
+		puts @first + " collects $" + @wages.to_s + " in wages."
+		@wages = 0
+	end 
+
+	def assignSupervisor(supervisor)
+		$employeeDirectory.each do |person|
+			if person.first == supervisor || person.first + " " + person.last == supervisor
+				@supervisor = person
+			end
+		end
+		if @supervisor.first == supervisor || @supervisor.first + " "  + @supervisor.last == supervisor
+			puts "#{@first} now reports to #{@supervisor.first} #{@supervisor.last}"
+		else
+			puts "could not find #{supervisor} is list of employees"
+		end
+	end
 end
 
 jack = Employee.new("Jack", "Fisher")
@@ -62,3 +87,11 @@ peter = Employee.new("Peter", "Hofstader")
 jack.about
 jill.about
 peter.about
+jack.work(8)
+jack.work(10)
+jack.collectPay
+jack.work(1.14)
+jack.collectPay
+p $employeeDirectory
+puts jack.first
+jack.assignSupervisor("Jill")
