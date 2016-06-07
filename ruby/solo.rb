08552c5a01b7726f5be5fb27411d89f107cd42b8
Kit Robinson <krobinson.dev@gmail.com>
2016-06-07 14:25:07 -0500

    inital class design commit

---
+++ ruby/solo.rb
@@ -0,0 +1,21 @@
+=begin
+
+Employee Class -----------------
+
+Properties of employees:--------
+first name
+last name
+phone number
+email address
+rate of pay
+supervisor
+direct reports []
+employee ID
+
+methods of employees------------ 
+update info
+arrive at work
+leave work
+collect pay
+
+end
