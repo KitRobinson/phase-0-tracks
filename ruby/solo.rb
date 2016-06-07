cb722c3ff1e5f68e711f72d5f7b57e22047ce450
Kit Robinson <krobinson.dev@gmail.com>
2016-06-07 14:50:21 -0500

    add class, attributes, get, set, init, and about methods

--- ruby/solo.rb
+++ ruby/solo.rb
@@ -5,17 +5,60 @@ Employee Class -----------------
 Properties of employees:--------
 first name
 last name
+title
 phone number
 email address
 rate of pay
 supervisor
 direct reports []
 employee ID
+wages owed
 
 methods of employees------------ 
 update info
-arrive at work
-leave work
+work
 collect pay
+assign to supervisor
+
+=end
+$nextID = 1
+
+class Employee
+
+	attr_reader :employeeID, :directReports, :wages
+	attr_accessor :supervisor, :first, :last, :eMail, :phone, :pay, :title
+
+	def initialize(first, last)
+		@first = first
+		@last = last
+		@title = "worker"
+		@employeeID = $nextID
+		$nextID = $nextID+1
+		@phone = "312-123-" + (@employeeID.to_s).rjust(4,'0')
+		@eMail = first[0] + last + "@company.com"
+		@supervisor = nil
+		@directReports = []
+		@pay = 10
+		@wages = 0
+	end
+
+	def about
+		puts @last + ", " + @first
+		if @supervisor != nil
+			puts @title + ", reporting to " + @supervisor
+		else
+			puts @title
+		end
+		puts "phone: " + @phone + ", email: " + @eMail
+		puts "pay rate: $" + @pay.to_s
+		puts "currently owed: $" + @wages.to_s + " in wages"  
+	end
 
 end
+
+jack = Employee.new("Jack", "Fisher")
+jill = Employee.new("Jill", "Harrison")
+peter = Employee.new("Peter", "Hofstader")
+jack.about
+jill.about
+peter.about
