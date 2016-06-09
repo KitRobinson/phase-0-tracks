24ddb259507da6a958f1561ef31d9c61d0bb2938
Kit Robinson <krobinson.dev@gmail.com>
2016-06-09 13:34:41 -0500

    change to mixin module and test with classes

--- ruby/shout.rb
+++ ruby/shout.rb
@@ -1,3 +1,4 @@
+=begin
 module Shout
 
 	def self.yell_angrily(words)
@@ -14,3 +15,33 @@ end
 
 puts Shout.yelling_happily("Hello")
 puts Shout.yell_angrily("Go Away")
+=end
+
+module Shout
+
+	def yell_angrily(words)
+		puts words + "!!! :("
+	end
+
+	def yell_happily(words)
+		puts words.upcase + "!!! :)"
+	end
+end
+
+class Neighbor 
+	include Shout
+end
+
+class Castaway
+	include Shout
+end
+
+neighbor = Neighbor.new
+neighbor.yell_happily("I love your yard art")
+neighbor.yell_angrily("but get if off my lawn")
+
+castaway = Castaway.new
+castaway.yell_angrily("I am trapped on this island")
+castaway.yell_happily("look, a ship!  I'm saved")
+castaway.yell_angrily("NoOOooOoOO it's sailing away")
+
