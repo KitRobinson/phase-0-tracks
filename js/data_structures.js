f94aefe8579117d0541270b258c57867f344902c
Kit Robinson <krobinson.dev@gmail.com>
2016-06-19 16:43:56 -0500

    add horse creation method

--- js/data_structures.js
+++ js/data_structures.js
@@ -17,3 +17,16 @@ add_color("Orange");
 
 console.log(colors);
 console.log(horse_names);
+
+var horse = {name: horse_names[0], color: colors[0]};
+var horses = [];
+horses += horse;
+
+console.log(horse);
+
+function create_horse(horse_seed){
+	return new_horse = {name: horse_names[horse_seed%horse_names.length], color: colors[horse_seed%colors.length]} 
+}
+
+console.log(create_horse(2));
+console.log(create_horse(486));
