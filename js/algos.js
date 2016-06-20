bbc59caf43c2cdd004d273d32a038d251c28ac3d
Kit Robinson <krobinson.dev@gmail.com>
2016-06-19 23:26:10 -0500

    add object congruence method and driver code test

--- js/algos.js
+++ js/algos.js
@@ -20,7 +20,37 @@ function longest_word(word_array){
 	return words[set_index];
 }
 
+//function that tests if there is a key/value match for two objects
+
+//take 2 objects as arguments
+function congruence(object_a, object_b){
+//------what if the 2 objects have different arrangements of keys and values (horse and car, for example, might both have speed)
+//to prevent this confusion, we could test every combination...
+	for (var key_a in object_a){
+	//for each key value pair in the first object
+		for (var key_b in object_b){
+		//for eacch key in the second object
+			if(key_a == key_b){
+			//if the keys are the same
+				if(object_a[key_a] == object_b[key_b]){
+				//if the values are the same
+					return true;
+				}
+			}
+		}
+	}
+	//return false if nothing returns true in the mathches.
+	return false;
+}
+
 var sample_a = ["ab", "abc", "abcd", "cd", "dbc"];
 console.log(longest_word(sample_a));
 var sample_b = ["cheese", "muffin", "mango", "milkshake", "apple"]
 console.log(longest_word(sample_b));
+var Cheese = {name: "Stilton", stinky: true};
+var Dog = {name: "Ed", stinky: true};
+var Horse = {name: "Ed", stinky: false};
+var County = {name: "Stilton", stinky: false};
+console.log(congruence(Cheese, Dog)); //expect true
+console.log(congruence(County, Dog)); //expect false
+console.log(congruence(Horse, County)); //expect true
