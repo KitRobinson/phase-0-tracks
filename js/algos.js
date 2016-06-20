eb505ae98f787493b848df8eff1824976cb448e7
Kit Robinson <krobinson.dev@gmail.com>
2016-06-19 23:39:37 -0500

    add random test data function and driver test code

--- js/algos.js
+++ js/algos.js
@@ -43,6 +43,32 @@ function congruence(object_a, object_b){
 	return false;
 }
 
+// a function which generates an array of random strings of a set length
+function random_test_data(length_of_test_array){
+// take an integer as argument
+	var test_array = [];
+	//create an empty array
+	var alphabet = "abcdefghijklmnopqurstuvwxyz";
+	var possible_chars = alphabet.split("");
+	//and an array of possible characters
+	for (i = 0; i < length_of_test_array; i++){
+	//for a number of times == to the int arg,
+		var test_string = ""
+		//create an empty string
+		var randD10 = Math.floor((Math.random() * 10) + 1);
+		//for a random number of times between 1 and 10
+		for (j = 0; j < randD10; j++){
+			test_string += possible_chars[Math.floor((Math.random() * 26) + 1)];
+			//generate a random letter
+			//append it to the new string
+		}
+		test_array.push(test_string);
+		//add this string to the array
+	}
+	return test_array;
+	//return the array
+}
+
 var sample_a = ["ab", "abc", "abcd", "cd", "dbc"];
 console.log(longest_word(sample_a));
 var sample_b = ["cheese", "muffin", "mango", "milkshake", "apple"]
@@ -54,3 +80,4 @@ var County = {name: "Stilton", stinky: false};
 console.log(congruence(Cheese, Dog)); //expect true
 console.log(congruence(County, Dog)); //expect false
 console.log(congruence(Horse, County)); //expect true
+console.log(random_test_data(4));
