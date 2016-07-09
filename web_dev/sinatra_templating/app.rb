87857147250934a3f2f45a93656fbc11ecd66692
Kit Robinson <krobinson.dev@gmail.com>
2016-07-09 13:18:10 -0500

    commit changes formerly missed

--- web_dev/sinatra_templating/app.rb
+++ web_dev/sinatra_templating/app.rb
@@ -1,6 +1,7 @@
 # require gems
 require 'sinatra'
 require 'sqlite3'
+require 'sinatra/reloader' if development?
 
 set :public_folder, File.dirname(__FILE__) + '/static'
 
@@ -13,8 +14,14 @@ get '/' do
   erb :home
 end
 
+#send to new students form
 get '/students/new' do
   erb :new_student
+end
+
+#send to transfer students form
+get '/students/transfer' do
+	erb :transfer_campus
 end
 
 # create new students via
@@ -23,5 +30,16 @@ post '/students' do
   db.execute("INSERT INTO students (name, campus, age) VALUES (?,?,?)", [params['name'], params['campus'], params['age'].to_i])
   redirect '/'
 end
+
+post '/new_campus' do
+	if db.execute("SELECT EXISTS(SELECT 1 FROM students WHERE name = ?)", [params['name']])
+		p "found student by name #{params[:name]}"	
+  		db.execute("UPDATE students SET campus = ? WHERE name = ?", [params['new_campus'], params['name']])
+  		p db.execute("SELECT * FROM students WHERE name = ?", params['name'])
+ 	else
+ 		p "did not find student by name"
+ 	end
+ 	redirect '/'
+ end
 
 # add static resources
